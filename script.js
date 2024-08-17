e643b732c62080e6aaeb853772ad76d445e1269f
amlan-git <AmlanB.2396@gmail.com>
2024-08-17 16:14:38 +0530

    JS change to add and remove counter

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const incrementbtn = document.querySelector('#increment');
 const decrementbtn = document.querySelector('#decrement');
 
+const ulElement = document.getElementById('list-items');
 let counter = 0;
 
 function incrementCounter(){
     const counterElement = document.getElementById('counter');
     counter++;
     counterElement.innerText = counter;
+
+    const li = document.createElement('li');
+    // const textNode = document.createTextNode('incremented by '+ counter);
+    // li.appendChild(textNode);
+
+    li.setAttribute('data-counter', counter);
+
+    li.innerHTML = 'Incremented by ' + counter;
+    ulElement.appendChild(li);
+
 }
 function decrementCounter(){
     const counterElement = document.getElementById('counter');
+    const li = ulElement.querySelector('[data-counter="'+counter+'"]');
+    li.remove();
+
+
     counter--;
     counterElement.innerText = counter;
 }
